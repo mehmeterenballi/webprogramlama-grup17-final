ddb5c5f0c4af67fba16343ab3228cd70db5d5829
mehmeterenballi <mehmeterenballi@hotmail.com>
2021-06-28 19:26:38 +0300

    Add files via upload

---
+++ ViewModels/CreateTitleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VeritabaniProjesi.ViewModel
+{
+    public class CreateTitleViewModel
+    {
+        public string PostTitle { get; set; }
+        public string Content { get; set; }
+        public string Sender { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VeritabaniProjesi.Models;
+
+namespace VeritabaniProjesi.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Announcement> Announcement { get; set; }
+    }
+}
